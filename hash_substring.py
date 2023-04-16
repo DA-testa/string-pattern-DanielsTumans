fdd428a83a8e8888352324805d365e560326756d
DanielsTumans <125546247+DanielsTumans@users.noreply.github.com>
2023-04-16 22:59:26 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,33 +1,33 @@
 # python3
 
 def read_input():
-    if "I" in input():
-        pat=input().strip()
-        txt=input().strip()
-    else:
-        with open("tests/06") as file:
-            pat=file.readline().strip()
-            txt=file.readline().strip()
-
-    return (pat, txt)
+      filek = input()
+      if filek == "I":
+         pattern = input().rstrip()
+         text = input().rstrip()
+      elif filek == "F":
+        with open ("tests/06") as f:
+         pattern = f.readline().rstrip()
+         text = f.readline().rstrip()
+      return (pattern, text)
+  
 
 def print_occurrences(output):
     print(' '.join(map(str, output)))
 
-def get_occurrences(pat, txt):
-    ot=[]
-    if len(pat) > len(txt):
-        return ot
+def get_occurrences(pattern, text):
+      result = []
+      if len(pattern) > len(text):
+         return result
+      patternhash = hash(pattern)
+      texthash = [hash(text[i:i+len(pattern)]) for i in range(len(text) - len(pattern)+1)]
+      for i in range (len(texthash)):
+         if patternhash == texthash[i] and text[i:i+len(pattern)] == pattern:
+            result.append(i)
+      return result
+         
+         
 
-    pat_hash = hash(pat)
-
-    text_hashes = [hash(txt[i:i+len(pat)]) for i in range(len(txt)-len(pat)+1)]
-
-    for i in range(len(text_hashes)):
-        if pat_hash == text_hashes[i] and txt[i:i+len(pat)] == pat:
-            ot.append(i)
-
-    return ot
-
+# this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))

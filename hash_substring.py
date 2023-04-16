70326463549d6be4386fdef36810bacfef0229b3
DanielsTumans <125546247+DanielsTumans@users.noreply.github.com>
2023-04-16 22:50:41 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -18,28 +18,17 @@ def print_occurrences(output):
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    result = []
-    p = 10**9+7
-    x=263
-    patternlength = len(pattern)
-    textlength = len(text)
-    pattern_hash = 0
-    for i in range(patternlength):
-       pattern_hash = (pattern_hash * x * ord(pattern[i])) % p
-    text_hash = 0
-    for i in range(patternlength):
-        text_hash = (text_hash * x * ord(text[i])) % p
-    if pattern_hash == text_hash and pattern == text[:patternlength]:
-        result.append(0)
-    x_pow = pow(x, patternlength -1, p)
-    for i in range(patternlength, textlength):
-        text_hash = (text_hash - ord(text[i-patternlength]) * x_pow) % p
-        text_hash = (text_hash * x + ord(text[i])) % p
-        if pattern_hash == text_hash and pattern == text[i - patternlength + 1:i + 1]:
-            result.append(i - patternlength + 1)
-    
-    return result
-
+      result = []
+      if len(pattern) > len(text):
+         return result
+      patternhash = hash(pattern)
+      texthash = [hash(text[i:i+len(pattern)]) for i in range(len(text) = len(pattern)+1)]
+      for i in range (len(texthash)):
+         if patternhash == texthash[i] and text[i:i+len(pattern)] == pattern:
+            result.append(i)
+      return result
+         
+         
 
 # this part launches the functions
 if __name__ == '__main__':

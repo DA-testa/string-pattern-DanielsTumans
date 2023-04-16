78714cddd8d0f41903686aee33300087c451e7fe
DanielsTumans <125546247+DanielsTumans@users.noreply.github.com>
2023-04-16 22:21:04 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -7,8 +7,8 @@ def read_input():
         text = input().rstrip()
    elif choice == "F":
     with open ("tests/test1.txt") as f:
-        pattern = f.readline()rstrip()
-        text = f.readline()rstrip()
+        pattern = f.readline().rstrip()
+        text = f.readline().rstrip()
     
     
     return (pattern, text)
